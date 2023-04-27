24b67db07ad8626d045b21d2db6e9947f03d6831
fagner <fagner.costa@ifam.edu.br>
2023-04-27 14:53:11 -0400

    Create Form in Modal

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ const daysText = document.querySelector('#days');
 const hoursText = document.querySelector('#hours');
 const minutesText = document.querySelector("#minutes");
 const secondsText = document.querySelector("#seconds");
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeModal = document.querySelector('.close-modal');
+const buttonSubscribe = document.querySelector("#btnSubscrible");
 
 const departureDate = new Date('31 Dec 2023');
 
@@ -16,8 +19,8 @@ function processCountDown(){
     const currentDate = new Date();
 
     let secondsTotal  = (departureDate - currentDate)/1000;
-    let days = Math.round((secondsTotal / 3600) /  24);
-    let hours = Math.round((secondsTotal / 3600) %  24);
+    let days = Math.floor((secondsTotal / 3600) /  24);
+    let hours = Math.floor((secondsTotal / 3600) %  24);
     let minutes = Math.floor((secondsTotal / 60) % 60);
     let seconds = Math.floor(secondsTotal % 60);
  
@@ -36,3 +39,11 @@ function renderCountdown(data){
     minutesText.textContent = `${format(minutes)}:`;
     secondsText.textContent = `${format(seconds)}`;
 }
+
+closeModal.addEventListener("click", ()=>{
+    modalOverlay.classList.toggle('active');
+});
+
+buttonSubscribe.addEventListener("click", ()=>{
+    modalOverlay.classList.toggle('active');
+});
